682f79ed2da5a8b21e8f8a77161d3c78a7337bb8
practicedemo67 <practicedemo67@gmail.com>
2019-09-16 15:33:58 +0530

    first commit R2.1 added

--- src/github/Practice.java
+++ src/github/Practice.java
@@ -3,25 +3,13 @@ package github;
 public class Practice {
 
 	public static void main(String[] args) {
-		System.out.println("version 1");
-
-		System.out.println("version 2");
-		
-
-		
 		System.out.println("version 2.1");
-
-		System.out.println("version 1.0");
-		
-
-		System.out.println("version 1.1");
 
 	}
 	public void newMethods() {
-		System.out.println("version 1.2");
+		System.out.println("version 2.1");
 		
-		
-		System.out.println("version 1.3");
+
 	}
 
 }
