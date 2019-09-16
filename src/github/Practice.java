0b39a29032b680756db744d704c74b293c9b401b
practicedemo67 <practicedemo67@gmail.com>
2019-09-16 15:40:53 +0530

    added 2.2. and 2.3

--- src/github/Practice.java
+++ src/github/Practice.java
@@ -5,9 +5,12 @@ public class Practice {
 	public static void main(String[] args) {
 		System.out.println("version 2.1");
 
+		System.out.println("version 2.3");
+		System.out.println("version 2.2");
 	}
 	public void newMethods() {
 		System.out.println("version 2.1");
+		System.out.println("version 2.3");
 		
 
 	}
